29025a70f2bdedd91bc224427d0b8e86ae62457d
ahmadumarse02 <ahmadumarse02@gmail.com>
2025-04-07 22:40:12 +0500

    layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/dashboard/ThemeProvider";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,8 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="hydrated" suppressHydrationWarning>
-      <body className={`${geistSans.className} ${geistMono.className}`}
-        cz-shortcut-listen="true"
+      <body cz-shortcut-listen="true"
       >
         <ThemeProvider
           attribute="class"
