dfb229c4224be84a09feb7545dd85165a3b8e835
ahmadumarse02 <ahmadumarse02@gmail.com>
2025-04-08 02:33:47 +0500

    uploadThing

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { ThemeProvider } from "@/components/dashboard/ThemeProvider";
+import { Toaster } from "@/components/ui/sonner";
 
 
 export const metadata: Metadata = {
@@ -24,6 +25,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
+          <Toaster richColors closeButton/>
         </ThemeProvider>
       </body>
     </html>
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "9t1a2s3pgv.ufs.sh",
+        port: ""
+      }
+    ]
+  }
 };
 
 export default nextConfig;
