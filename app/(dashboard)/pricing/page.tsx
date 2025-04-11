2efb7f20afb90e66e44d91b1ab16b5a06fefc848
ahmadumarse02 <ahmadumarse02@gmail.com>
2025-04-12 00:25:17 +0500

    submit button

--- app/(dashboard)/pricing/page.tsx
+++ app/(dashboard)/pricing/page.tsx
@@ -3,9 +3,9 @@ import React from "react";
 const PricingPage = () => {
   return (
     <>
-    <div className="text-xl">Hello world</div>
+      <div className="text-xl">Hello world</div>
     </>
-  )
+  );
 };
 
 export default PricingPage;
