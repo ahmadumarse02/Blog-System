da72f2984f624418393805e5154cbd1e4a84ec69
ahmadumarse02 <ahmadumarse02@gmail.com>
2025-04-12 00:09:08 +0500

    site action

--- actions/createSiteAction.ts
+++ actions/createSiteAction.ts
@@ -4,14 +4,24 @@ import { redirect } from "next/navigation";
 import { parseWithZod } from "@conform-to/zod";
 
 import prisma from "@/utils/prisma";
-import { siteSchema } from "@/schemas/siteSchema";
 import { requireUser } from "@/utils/requireUser";
+import { SiteCreateSchema } from "@/schemas/siteCreateSchema";
 
 export async function CreateSiteAction(prvState: unknown, formdata: FormData) {
   const user = await requireUser();
 
-  const submission = parseWithZod(formdata, {
-    schema: siteSchema,
+  const submission = await parseWithZod(formdata, {
+    schema: SiteCreateSchema({
+      async isSubdirectoryUniqe() {
+        const exitingSubDirectory = await prisma.site.findUnique({
+          where: {
+            subdirectory: formdata.get("subdirectory") as string,
+          },
+        });
+        return !exitingSubDirectory;
+      },
+    }),
+    async: true,
   });
 
   if (submission.status !== "success") {
@@ -29,3 +39,35 @@ export async function CreateSiteAction(prvState: unknown, formdata: FormData) {
 
   return redirect("/sites");
 }
+
+// "use server";
+
+// import { redirect } from "next/navigation";
+// import { parseWithZod } from "@conform-to/zod";
+
+// import prisma from "@/utils/prisma";
+// import { siteSchema } from "@/schemas/siteSchema";
+// import { requireUser } from "@/utils/requireUser";
+
+// export async function CreateSiteAction(prvState: unknown, formdata: FormData) {
+//   const user = await requireUser();
+
+//   const submission = parseWithZod(formdata, {
+//     schema: siteSchema,
+//   });
+
+//   if (submission.status !== "success") {
+//     return submission.reply();
+//   }
+
+//   await prisma.site.create({
+//     data: {
+//       name: submission.value.name,
+//       subdirectory: submission.value.subdirectory,
+//       description: submission.value.description,
+//       userId: (await user).id,
+//     },
+//   });
+
+//   return redirect("/sites");
+// }
