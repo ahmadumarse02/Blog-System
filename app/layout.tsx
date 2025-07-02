916664007045a5363c61cc91f8bf3f6d0845564d
Ahmad Umar <ahmadumarse02@gmail.com>
2025-07-02 11:13:37 +0500

    add vercel analytics on that site

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { ThemeProvider } from "@/components/dashboard/ThemeProvider";
 import { Toaster } from "@/components/ui/sonner";
+import { Analytics } from '@vercel/analytics/next';
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,6 +24,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
+          <Analytics />
           <Toaster richColors closeButton />
         </ThemeProvider>
       </body>
